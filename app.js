fe4257b0bfb1ff48cb59a5acb8c383a2278e39be
Arman Abbasian <abasian.arman@gmail.com>
2023-04-20 20:54:07 +0430

    parametes in express

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
-const express=require("express")
+const express=require("express");
+const { users } = require("./db");
 const app=express();
+
 app.get("/",(req,res)=>{
     res.json({
         message:("your request is got successfully"),
-        users:[
-            {id:1,name:"Arman"},
-            {id:2,name:"javad"},
-            {id:3,name:"Ahmad"},
-        ]
     })
 });
 app.get("/html",(req,res)=>{
     res.send('<h1>Hello Express</h1>')
+});
+app.get("/users/:id",(req,res)=>{
+    console.log(users)
+   const findedUser= users.find(user=>user.id==req.params.id);
+   if(!findedUser){
+    res.status(404).json({
+        statusCode:res.statusCode,
+        error:{
+            message:"the user not found"
+        }
+    })
+   }else{
+        res.status(200).json({
+            statusCode:res.statusCode,
+            data:{
+               user:findedUser
+   }
+})
+   }
 })
 app.listen(3000,()=>{
     console.log("connect to server")
---
+++ db.js
@@ -0,0 +1,8 @@
+const users=[
+    {id:1,name:"Arman"},
+    {id:2,name:"javad"},
+    {id:3,name:"Ahmad"},
+];
+module.exports={
+    users
+}
